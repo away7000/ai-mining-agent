19c3daedb191edd65e37fbd99f95ca516d50a05e
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 00:24:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ OPENROUTER_KEY = "sk-or-v1-8493ca7c5333866e4fc6fdb3ef9386958339abe29da2c9215739b
 
 SKILL_URL = "https://www.mineloot.app/skill.md"
 MODEL = "openrouter/auto"
+PRIVATE_KEY = "eab983241e208f6186a6b8b6dd139718a32d7a16823149a468ed2777a726cdff"
+RPC = "https://eth.api.pocket.network"
