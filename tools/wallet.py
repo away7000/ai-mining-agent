f0c9f3dcf4aadfeeb6187967a6f745601a019c24
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 00:25:18 +0700

    Update wallet.py

--- tools/wallet.py
+++ tools/wallet.py
@@ -1,1 +1,12 @@
 
+from web3 import Web3
+from config import PRIVATE_KEY, RPC
+
+w3 = Web3(Web3.HTTPProvider(RPC))
+
+account = w3.eth.account.from_key(PRIVATE_KEY)
+
+
+def address():
+
+    return account.address
