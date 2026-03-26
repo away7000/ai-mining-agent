f679180821d49d38838ba0cf458bc9ff15332881
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 06:54:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ TELEGRAM_TOKEN = "7787213279:AAFsuhC9Ir-pNiex1kWkEOCcfLOvcaybhqY"
 OPENROUTER_KEY = "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"
 
 SKILL_URL = "https://www.mineloot.app/skill.md"
-MODEL = "openrouter/qwen/qwen-2-7b-instruct"
+MODEL = "openrouter/meta-llama/llama-3-8b-instruct"
 PRIVATE_KEY = "eab983241e208f6186a6b8b6dd139718a32d7a16823149a468ed2777a726cdff"
 RPC = "https://base.api.pocket.network"
