6288939927bfba3fa3e17ef5dd4ed1b389299cd3
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 00:25:30 +0700

    Update mining_tool.py

--- tools/mining_tool.py
+++ tools/mining_tool.py
@@ -1,1 +1,10 @@
+from tools.http_tool import http_get, http_post
 
+
+def execute_call(url, method="GET", data=None):
+
+    if method == "GET":
+        return http_get(url)
+
+    if method == "POST":
+        return http_post(url, data)
