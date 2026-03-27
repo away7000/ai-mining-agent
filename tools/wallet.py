b1bcf8295a4123b733c8a55b8d5a4a074328dea0
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 07:06:04 +0700

    Update wallet.py

--- tools/wallet.py
+++ tools/wallet.py
@@ -10,3 +10,9 @@ account = w3.eth.account.from_key(PRIVATE_KEY)
 def address():
 
     return account.address
+    
+def balance():
+
+    bal = w3.eth.get_balance(account.address)
+
+    return w3.from_wei(bal, "ether")
