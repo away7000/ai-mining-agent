84038ec262199a1711c55c98225f91f8a5df2441
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 09:14:18 +0700

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,7 +1,7 @@
 import time
 import requests
 from skill_parser import parse_skill
-from miner import run_mining, auto_mining_loop
+
 
 def run_mining():
 
@@ -9,7 +9,7 @@ def run_mining():
 
     urls = parse_skill()
 
-    print(urls)
+    print("URLS:", urls)
 
     for url in urls:
 
@@ -19,12 +19,11 @@ def run_mining():
 
             r = requests.get(url, timeout=10)
 
-            print("RESP:", r.status_code)
+            print("RESP", r.status_code)
 
         except Exception as e:
 
             print("ERR", e)
-
 
 
 def auto_mining_loop():
