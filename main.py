55894d4ab1bec5e485c1f594cd205314e50e7df9
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 07:01:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,33 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from config import TELEGRAM_TOKEN
-from agent import ask_ai
-from loop import auto_loop
-from miner import auto_mining_loop
-import threading
-from tools.wallet import address
 
-AUTO = False
 
-async def wallet(update, ctx):
-
-    addr = address()
-
-    await update.message.reply_text(
-        f"Wallet:\n{addr}"
-    )
-    
 async def start(update, ctx):
-    await update.message.reply_text("Bot ready")
+    await update.message.reply_text("OK start")
 
 
 async def mine(update, ctx):
-
-    res = ask_ai("mine")
-
-    await update.message.reply_text(res)
+    await update.message.reply_text("OK mine")
 
 
 async def claim(update, ctx):
-
-    res = ask_ai("claim")
-
-    await update.message.reply_text(res)
-
-
-async def status(update, ctx):
-
-    res = ask_ai("status mining")
-
-    await update.message.reply_text(res)
-
-
-def loop_runner():
-
-    auto_loop()
+    await update.message.reply_text("OK claim")
 
 
 async def auto(update, ctx):
-
-    global AUTO
-
-    if AUTO:
-        await update.message.reply_text("already running")
-        return
-
-    AUTO = True
-
-    threading.Thread(
-        target=loop_runner,
-        daemon=True
-    ).start()
-
-    await update.message.reply_text("AUTO MINING START")
+    await update.message.reply_text("OK auto")
 
 
 async def stop(update, ctx):
+    await update.message.reply_text("OK stop")
 
-    global AUTO
 
-    AUTO = False
-
-    await update.message.reply_text("AUTO STOP")
+async def status(update, ctx):
+    await update.message.reply_text("OK status")
 
 
 async def handle(update, ctx):
-
-    text = update.message.text
-
-    res = ask_ai(text)
-
-    await update.message.reply_text(res)
+    await update.message.reply_text("TEXT")
 
 
 app = Application.builder().token(
@@ -93,13 +41,8 @@ app.add_handler(CommandHandler("claim", claim))
 app.add_handler(CommandHandler("auto", auto))
 app.add_handler(CommandHandler("stop", stop))
 app.add_handler(CommandHandler("status", status))
-app.add_handler(CommandHandler("wallet", wallet))
 
 app.add_handler(MessageHandler(filters.TEXT, handle))
 
-threading.Thread(
-    target=auto_mining_loop,
-    daemon=True
-).start()
 
 app.run_polling()
