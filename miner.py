7234a47783f81f3b996708ee778ce6455f403e21
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 12:55:29 +0700

    Update miner.py

--- miner.py
+++ miner.py
@@ -2,25 +2,37 @@ import time
 import requests
 from skill_parser import parse_skill
 from tools.wallet import address
+from tools.wallet import address, get_account
+from eth_account.messages import encode_defunct
 
 def run_mining():
 
-    print("WALLET:", address())
-
     urls = parse_skill()
+
+    addr = address()
+
+    msg, sig = sign_message()
 
     for url in urls:
 
         try:
 
-            r = requests.get(url, timeout=10)
+            r = requests.post(
+                url,
+                json={
+                    "wallet": addr,
+                    "message": msg,
+                    "signature": sig
+                },
+                timeout=10
+            )
 
             print(r.text)
 
         except Exception as e:
 
             print(e)
-
+            
 def auto_mining_loop():
 
     while True:
@@ -28,3 +40,15 @@ def auto_mining_loop():
         run_mining()
 
         time.sleep(20)
+        
+def sign_message():
+
+    acc = get_account()
+
+    msg = str(int(time.time()))
+
+    message = encode_defunct(text=msg)
+
+    signed = acc.sign_message(message)
+
+    return msg, signed.signature.hex()
