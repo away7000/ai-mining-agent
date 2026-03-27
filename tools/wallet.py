79cd066897ba0e9cc7643481eb53d518b840f88f
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-27 12:41:13 +0700

    Update wallet.py

--- tools/wallet.py
+++ tools/wallet.py
@@ -1,5 +1,5 @@
-from web3 import Web3
 import os
+from web3 import Web3
 
 PRIVATE_KEY = os.environ["PRIVATE_KEY"]
 RPC = os.environ["RPC"]
@@ -10,11 +10,13 @@ account = w3.eth.account.from_key(PRIVATE_KEY)
 
 
 def address():
-
     return account.address
-    
+
+
 def balance():
-
     bal = w3.eth.get_balance(account.address)
-
     return w3.from_wei(bal, "ether")
+
+
+def get_account():
+    return account
